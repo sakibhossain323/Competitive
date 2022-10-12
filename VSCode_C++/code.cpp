ebc0bf4372cdce0dc0d17f802fb7b8d7058ea551
unknown <sakibhossain323@gmail.com>
2022-10-12 18:41:32 +0600

    updated template

--- VSCode_C++/code.cpp
+++ VSCode_C++/code.cpp
@@ -3,16 +3,18 @@ using namespace std;
 
 using ll = long long;
 #define MOD 1000000007
-#define MAX 100001
-
+#define MAX 10001
 
 
 void solve(int tcase)
 {
-    int n;
-    cin >> n;
+    // int n;
+    // cin >> n;
 
     //
+    // vector <int> a(n);
+    // for(int i = 0; i < n; i++) cin >> a[i];
+    
 
 
     //printf("Case #%d: %d\n", tcase, n);
@@ -25,24 +27,30 @@ int main ()
 //----------------------------------------------------------------
 
 
-    // #ifndef ONLINE_JUDGE
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-    // #endif
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
 
  
 //----------------------------------------------------------------
 
 
-    int t;
-    cin >> t;
-    for(int tcase = 1; tcase <= t; tcase++) solve(tcase);
+    // int t;
+    // cin >> t;
+    // for(int tcase = 1; tcase <= t; tcase++) solve(tcase);
     
 
 //----------------------------------------------------------------
 
     //
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < n; i++) cout << a[i] << endl;
     
-    
+
+
     return 0;
 }
