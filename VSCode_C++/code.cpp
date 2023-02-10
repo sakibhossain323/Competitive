65a27673b8cc4eb8608988dc92b9bc8dcda211c7
unknown <sakibhossain323@gmail.com>
2023-02-10 19:48:48 +0600

    Updated Snippet

--- VSCode_C++/code.cpp
+++ VSCode_C++/code.cpp
@@ -10,52 +10,17 @@ using ll = long long;
 #define MAX 1000001
 
 
-bool isprime[MAX];
-vector <int> primes;
-
-void sieve()
-{
-    isprime[2] = true;
-    for(int i = 3; i < MAX; i+=2) isprime[i] = true;
-    for(int i = 2; i*i < MAX; i++)
-    {
-        if(isprime[i])
-        {
-            for(int j = i*i; j < MAX; j+=i) isprime[j] = false;
-        }
-    }
-
-    for(int i = 1; i < MAX; i++) if(isprime[i]) primes.push_back(i);
-}
 
 
 void solve(int tcase)
 {
-    // int n;
-    // cin >> n;
-    //
-    // vector <int> a(n);
-    // for(int i = 0; i < n; i++) cin >> a[i];
-    
-    ll n;
+    int n;
     cin >> n;
+    
+    vector <int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
 
-    ll nod = 1;
-    for(int i: primes)
-    {
-        int cur_pow = 0;
-        if(i*i > n) break;
-        while(n%i==0)
-        {
-            cur_pow++;
-            n/=i;
-        }
-        nod*= (cur_pow+1);
-    }
-
-    if(n != 1) nod*=2;
-
-    caseout(tcase) << nod-1 << nln;    
+    
 }
 
 
@@ -74,18 +39,12 @@ int main ()
     
     local_io();
 
-    sieve();
 
     //---------------------------------------------------
 
-    int t;
+    int t = 1;
     cin >> t;
     for(int tcase = 1; tcase <= t; tcase++) solve(tcase);
 
-    //---------------------------------------------------
-    
-
-    
-    
     return 0;
 }
