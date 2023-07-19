df5766b949b03a63baac8ec7388e409aff96aa41
sakibhossain323 <sakibhossain323@gmail.com>
2023-07-20 01:18:32 +0600

    Created sublime text workspace

---
+++ Sublime_cpp/code.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define nln "\n"
+#define caseout(n) cout << "Case " << n << ": "
+
+
+const ll MOD = 1e9+7;
+const int MAX = 1e7+1;
+
+
+
+
+
+void solve(int tcase)
+{
+    ll  n;
+    cin >> n;
+
+    // ll m;
+    // cin >> m;
+
+
+    // vector <ll> a(n);
+    // for(int i= 0; i < n; i++) cin >> a[i];
+
+    
+    caseout(tcase)  << n*n;
+
+
+    cout << nln;
+    
+}
+
+
+
+signed main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    //---------------------------------------------------
+    
+    
+    
+    //---------------------------------------------------
+
+    int t = 1;
+    cin >> t;
+    for(int tcase = 1; tcase <= t; tcase++) solve(tcase);
+
+    return 0;
+}
