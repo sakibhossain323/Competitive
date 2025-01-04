d2915b55d3be51f55d0c18d9adb7223510fed886
sakibhossain323 <sakibhossain323@gmail.com>
2025-01-05 02:45:09 +0600

    update template

--- Sublime_cpp/code.cpp
+++ Sublime_cpp/code.cpp
@@ -1,48 +1,44 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-
+using namespace __gnu_pbds;
+template <typename DT> 
+using ordered_set = tree <DT, null_type, less<DT>, rb_tree_tag, tree_order_statistics_node_update>;
 using ll = long long;
-#define caseout(n) cout << "Case " << n << ": "
 
-const ll MOD = 998244353;//1e9+7;
+
+#define tcout(i) cout << "Case " << i << ": "
+const ll MOD = 1e9+7;
 const ll INF = 1e18;
-const int MAX = 1e5+1;
+const int MAX = 1e6+5;
 
 
 
 
-
-void solve(int tcase)
-{
-    ll n;
-    cin >> n;
-
-    // ll m;
-    // cin >> m;
-
-    // vector<ll> a(n);
-    // for(int i = 0; i < n; i++) cin >> a[i];
-
-
-    caseout(tcase) << n*n;
+void solve(int tc)
+{   
+    ll n; cin >> n;
+    tcout(tc) << n*n;
     
 
-    cout << "\n";
-}
 
+
+    cout << '\n';
+}
 
 
 signed main()
 {
     cin.tie(0)->sync_with_stdio(0);
-    
+
 
 
 
 
     int t = 1;
     cin >> t;
-    for(int tcase = 1; tcase <= t; tcase++) solve(tcase);
+    for(int i = 1; i <= t; i++) solve(i);
 
     return 0;
 }
--- Sublime_cpp/test.cpp
+++ Sublime_cpp/test.cpp
@@ -1,43 +1,42 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-
+using namespace __gnu_pbds;
+template <typename DT> 
+using ordered_set = tree <DT, null_type, less<DT>, rb_tree_tag, tree_order_statistics_node_update>;
 using ll = long long;
-#define nln "\n"
-#define caseout(n) cout << "Case " << n << ": "
 
 
+#define tcout(i) cout << "Case " << i << ": "
 const ll MOD = 1e9+7;
-const int MAX = 2e5+1;
+const ll INF = 1e18;
+const int MAX = 2e6+5;
 
 
 
 
+void solve(int tc)
+{   
+    cout << "testing...";
 
-int main ()
-{
-    cout << "testing..." << nln;
+                
 
-    
-
-    return 0;
+    cout << '\n';
 }
 
 
-
-/*
-
-
-
-==================================================================================
+signed main()
+{
+    cin.tie(0)->sync_with_stdio(0);
 
 
 
-==================================================================================
 
 
+    int t = 1;
+    // cin >> t;
+    for(int i = 1; i <= t; i++) solve(i);
 
-==================================================================================
-
-
-
-*/
+    return 0;
+}
